2a3bc8295d04dbe4536481abf05f952a2b8c2382
Sinerock <samuelmoyalcc@gmail.com>
2025-01-08 19:38:51 -0500

    #00 Sinerock.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Sinerock.py
@@ -0,0 +1,27 @@
+#EJ1
+#Esto es un comentario,la pagina de mi lenguaje https://www.python.org/
+ 
+"""Esto tambien es umn comentario"""
+
+"""
+Esto 
+es
+un
+comentario
+en vaerias
+lineas
+"""
+
+#EJ3
+
+Mi_Variable ="epico"
+
+#Ej4
+#int "Entero"
+A = 32
+#float "Flotante"
+B = 23.4
+#str "Cadenas"
+c = "Hola, mundo"
+
+print = "Hola,Python"
